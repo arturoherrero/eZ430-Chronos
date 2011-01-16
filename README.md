dfe8b588f870135a1c3fe305c515fa8f3daf8ec1
Arturo Herrero <arturo.herrero@gmail.com>
2011-01-16 02:52:29 +0100

    Get button data from watch

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 A collection of scripts for interacting with the eZ430 Chronos or using the eZ430 to interact with other devices.
 
-### Current scripts:
+## Current scripts:
   eZ430.py    Class that provides the communication with the USB device.
   ppt.py      Get button data from Chronos watch.
 
--- eZ430.py
+++ eZ430.py
@@ -1,25 +1,31 @@
 #!/usr/bin/env python
+
 import serial
 
 class watch():
-        def __init__(self, dev = "/dev/ttyACM0", deb = 50):
-                self.dev = dev
-                self.deb = deb
-		self.start()
-	def start(self):
-                self.conn = serial.Serial(self.dev, 115200, timeout = 1)
-                self.write("\xFF\x07\x03")
-	def stop(self):
-		self.write("\xFF\x09\x03")
-		self.conn.close()
-        def write(self,msg):
-                self.conn.write(msg)
-        def read(self, len = 7):
-                self.conn.write("\xFF\x08\x07\x00\x00\x00\x00")
-                return self.conn.read(len)
-        def debounce(self):
-                i=self.deb
-                while i:
-                        self.read()
-                        i-=1
-	
+    
+    def __init__(self, dev = "/dev/ttyACM0", deb = 50):
+        self.dev = dev
+        self.deb = deb
+        self.start()
+        
+    def start(self):
+        self.conn = serial.Serial(self.dev, 115200, timeout = 1)
+        self.write("\xFF\x07\x03")
+        
+    def stop(self):
+        self.write("\xFF\x09\x03")
+        self.conn.close()
+        
+    def write(self, msg):
+        self.conn.write(msg)
+        
+    def read(self, len = 7):
+        self.conn.write("\xFF\x08\x07\x00\x00\x00\x00")
+        return self.conn.read(len)
+    
+    def debounce(self):
+        i = self.deb
+        while i:
+            self.read()
+            i -= 1
